1c622caa1a0fb683b382960155a7c16dadfd7162
ttak0422 <bgdaewalkman@gmail.com>
2019-02-25 14:05:29 +0900

    initial commit

---
+++ BlackJack/BlackJack.Tests/Expecto.fs
@@ -0,0 +1,72 @@
+﻿open Expecto
+open BlackJack
+
+[<Tests>]
+let modelTests =
+  testList "model test" [
+    test "デッキの枚数" {      
+      Expect.equal 52 deck.Length "52枚であるはず"
+    }
+    test "suit ace" {      
+      Expect.equal aceForTest suit.[0] "suit.[0] = ace"
+    }
+    test "suit jqk" {
+      Expect.equal jackForTest suit.[11 - 1] "suit.[10] = jack"
+      Expect.equal queenForTest suit.[12 - 1] "suit.[11] = queen"
+      Expect.equal kingForTest suit.[13 - 1] "suit.[12] = king"      
+    }
+    test "suit number" {
+      for i in 2 .. 10 do
+        let card = suit.[i - 1]
+        let expected = cardForTestData i
+        let msg = sprintf "suit.[%i] = [%i]" (i-1) i                         
+        Expect.equal expected card msg
+    }
+    test "unknown number" {
+      Expect.throws (fun _ -> suit.[14] |> ignore) "14番目のカードは存在しないよ"      
+    }
+  ]
+  
+[<Tests>]
+let logicTests =
+  
+  let ace = suit.[0]
+  let card2 = suit.[2 - 1]
+  let card3 = suit.[3 - 1]
+  let jack = suit.[11 - 1]
+  let queen = suit.[12 - 1]
+  let king = suit.[13 - 1]
+  
+  testList "logic test" [
+    test "カードが与えられないとき" {
+      let expected = 0
+      let result = sumHand []
+      Expect.equal expected result "0であるべき"
+    }
+    test "カードの合計" {
+      let expected = 5
+      let result = sumHand [card3; card2]
+      Expect.equal expected result "3 + 2 = 5"
+    }
+    test "J,Q,Kが10として扱われる" {            
+      Expect.equal (sumHand [suit.[2 - 1]; jack]) 12 "2 + ? = 12"
+      Expect.equal (sumHand [suit.[3 - 1]; jack]) 13 "3 + ? = 13"
+      Expect.equal (sumHand [jack; queen]) 20 "j + q = 20"
+      Expect.equal (sumHand [queen; king]) 20 "q + k = 20"
+      Expect.equal (sumHand [king; jack]) 20 "k + j = 20"
+    }
+    test "Aceが1と11をとる" {
+      Expect.equal (sumHand [suit.[2 - 1]; ace]) 13 "ace as 11"
+      Expect.equal (sumHand [jack; ace]) 21 "ace as 11"
+      Expect.equal (sumHand [jack; ace; suit.[10 - 1]]) 21 "ace as 1"
+      Expect.equal (sumHand [ace; ace; king; suit.[9 - 1]]) 21 "ace as 1"
+    }
+    test "手札の合計が21を超えたとき21以上の値が返される" {
+      Expect.isGreaterThan (sumHand [jack; king; suit.[5 - 1]]) 21 "jack + king + 5 > 21" 
+    }
+  ]  
+
+[<EntryPointAttribute>]
+let main args =
+  runTestsInAssembly defaultConfig args 
+       
---
+++ BlackJack/BlackJack.fs
@@ -0,0 +1,52 @@
+﻿module BlackJack
+
+type Card =
+  private
+  | Ace
+  | Number of int
+  | Jack
+  | Queen
+  | King
+  
+let cardForTestData = Number
+let aceForTest = Card.Ace
+let jackForTest = Card.Jack
+let queenForTest = Card.Queen
+let kingForTest = Card.King
+
+let suit =
+  [ Ace ]
+  @ [ for i in 2 .. 10 -> Number i ]
+  @ [ Jack; Queen; King ]
+  
+let heartSuit = suit
+let diaSuit = suit
+let cloverSuit = suit
+let spadeSuit = suit
+
+let deck =
+  heartSuit
+  @ diaSuit
+  @ cloverSuit
+  @ spadeSuit
+
+let toPoint = function
+  | Ace -> [ 1; 11 ]
+  | Number n -> [ n ]
+  | _ -> [ 10 ]
+  
+let plusEach list1 list2 =
+  List.collect (fun element1 ->
+    List.map (fun element2 ->
+      element1 + element2
+    ) list2
+  ) list1
+      
+let sumHand cards =
+  let possiblePoints = cards |> List.map toPoint
+  let scoreCandidates = possiblePoints |> List.fold plusEach [0]
+  scoreCandidates
+  |> List.where ((>=) 21)
+  |> function
+  | [] -> scoreCandidates |> List.min
+  | noBusts -> noBusts |> List.max
